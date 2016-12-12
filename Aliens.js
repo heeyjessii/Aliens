7636a808b42e58c048f68cb0de9d1837f831916c
Jessica <11lopez.jessica@gmail.com>
2016-12-12 08:14:31 -1000

    ok

---
+++ Aliens.js
@@ -0,0 +1,16 @@
+var aliens = 10000;
+var red = 2500;
+var blue = 15000;
+var green = 6000;
+
+if (blue + red + green == aliens) {
+  if (green > 5000) {
+    console.log("Ugh, why are they so ugly?")
+  }
+  if (green + red > 8000) {
+    console.log("No, Why????")
+  }
+  if (red + blue < 3000) {
+    console.log("Okay, this seems somewhat managable. Wait no.. We're still dead...")
+  }
+}
